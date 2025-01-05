9aa6dfe956873374c98e462ee07e8eb093d9ed4c
mntc3434 <mnte3434@gmail.com>
2025-01-05 10:42:11 +0300

    update game name

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # be_up
+
+#this is simple text to cheak the connection
+#..blab bla
---
+++ try.cpp
@@ -0,0 +1,6 @@
+#includ <iostream>
+using namespace std;
+
+int main(){
+    cout<<"hey this is cpp code for trying pull request";
+}
